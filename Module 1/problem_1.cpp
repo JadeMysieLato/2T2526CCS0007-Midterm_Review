e50fed4033007c838457d0ecd88e7e15e233346e
Jade Mysie Lato <jademysie@gmail.com>
2026-02-07 12:55:54 +0800

    created and added the instructions to problem_1.cpp

---
+++ Module 1/problem_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*
+  Create a user-defined function that takes two integers as parameters and returns their sum.
+
+  Harder Problem: --- see problem_1-1.cpp for the answer
+  In the function, add the sum into the parameter.
+  Note the use of bitwise operator (&).
+*/
+
+int main()
+{
+  
+
+  return 0;
+}
